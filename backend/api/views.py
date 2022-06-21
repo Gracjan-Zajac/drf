8a1c5d90ebdba4c4f0945265505f2eaecd5a43bf
Gracjan-Zajac <gracjan.zajac@hotmail.com>
2022-06-21 20:40:19 +0200

    api/

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,11 +4,14 @@ from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
     # request -> Django HttpRequest instance
+    print(request.GET)  # URL query params
     body = request.body  # byte string of JSON data
     data = {}
     try:
         data = json.loads(body)
     except:
         pass
-    print(data)
-    return JsonResponse({'message': 'Hi, this is your Django API response!'})
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,9 +1,9 @@
 import requests
 
 # endpoint = 'https://httpbin.org/status/200'
-endpoint = 'http://localhost:8000/api'  # http://127.0.0.1:8000/
+endpoint = 'http://localhost:8000/api/'  # http://127.0.0.1:8000/
 
 get_response = requests.get(endpoint, params={'abc': 123}, json={'query': 'Hello world!'})  # HTTP response
-print(get_response.text)  # print raw text response
+# print(get_response.text)  # print raw text response
 # print(get_response.status_code)
 print(get_response.json())
