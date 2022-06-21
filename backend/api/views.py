84b76acd1635fef3c0a095c159051a62b94fb7f1
Gracjan-Zajac <gracjan.zajac@hotmail.com>
2022-06-21 20:17:47 +0200

    print request body

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,14 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
+    # request -> Django HttpRequest instance
+    body = request.body  # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
     return JsonResponse({'message': 'Hi, this is your Django API response!'})
