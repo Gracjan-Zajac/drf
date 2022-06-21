d98d858f10420c8bdca50c7eebdc120bcd91539b
Gracjan-Zajac <gracjan.zajac@hotmail.com>
2022-06-21 19:45:30 +0200

    json response

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,9 @@
 import requests
 
 # endpoint = 'https://httpbin.org/status/200'
-endpoint = 'http://localhost:8000/'  # http://127.0.0.1:8000/
+endpoint = 'http://localhost:8000/api'  # http://127.0.0.1:8000/
 
 get_response = requests.get(endpoint, data={'json': 'Hello world!'})  # HTTP response
 print(get_response.text)  # print raw text response
 print(get_response.status_code)
+print(get_response.json()['message'])
