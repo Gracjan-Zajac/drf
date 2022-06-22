3f74f75765cbb309f7e1e84f55619ad0e7637807
Gracjan-Zajac <gracjan.zajac@hotmail.com>
2022-06-22 20:32:56 +0200

    django model instance to dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,15 @@
 import json
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from django.http import JsonResponse, HttpResponse
+from products.models import Product
 
 
 def api_home(request, *args, **kwargs):
-    # request -> Django HttpRequest instance
-    print(request.GET)  # URL query params
-    body = request.body  # byte string of JSON data
+    model_data = Product.objects.all().order_by('?').first()
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    if model_data:
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
     return JsonResponse(data)
+    #     print(data)
+    #     json_data_str = json.dumps(data)
+    # return HttpResponse(json_data_str, content_type='application/json')
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,8 @@ import requests
 # endpoint = 'https://httpbin.org/status/200'
 endpoint = 'http://localhost:8000/api/'  # http://127.0.0.1:8000/
 
-get_response = requests.get(endpoint, params={'abc': 123}, json={'query': 'Hello world!'})  # HTTP response
+get_response = requests.get(endpoint, json={'product_id': '123'})  # HTTP response
+# print(get_response.headers)
 # print(get_response.text)  # print raw text response
 # print(get_response.status_code)
 print(get_response.json())
