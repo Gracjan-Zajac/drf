8e2fd30c33964cbe599c6e9bc1faf1fd41a2f3ad
Gracjan-Zajac <gracjan.zajac@hotmail.com>
2022-06-23 20:59:49 +0200

    ingest data with drf views

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,7 @@ import requests
 # endpoint = 'https://httpbin.org/status/200'
 endpoint = 'http://localhost:8000/api/'  # http://127.0.0.1:8000/
 
-get_response = requests.get(endpoint, json={'product_id': '123'})  # HTTP response
+get_response = requests.post(endpoint, json={'title': 'Hi!', 'price': 123, 'content': 'Hello world!'})  # HTTP response
 # print(get_response.headers)
 # print(get_response.text)  # print raw text response
 # print(get_response.status_code)
