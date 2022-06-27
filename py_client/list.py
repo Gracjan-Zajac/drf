00cf27dddde7e40a59ff4145df09d6d8783f71bc
Gracjan-Zajac <gracjan.zajac@hotmail.com>
2022-06-27 19:28:57 +0200

    list client

--- py_client/list.py
+++ py_client/list.py
@@ -2,8 +2,7 @@ import requests
 
 endpoint = "http://localhost:8000/api/products/"
 
-data = {"title": "This one is done", "content": "Let's see..."}
 
-get_response = requests.get(endpoint, json=data)
+get_response = requests.get(endpoint)
 
 print(get_response.json())
