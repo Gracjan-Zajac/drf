1284bc74836e688ca36f3a642c0b386dd4477b7e
Gracjan <gracjan.zajac@hotmail.com>
2022-07-13 21:10:44 +0200

    get edit url

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,20 +1,33 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name="product-detail", lookup_field="pk")
+    edit_url = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Product
         fields = [
+            'pk',
             'title',
             'content',
             'price',
             'sale_price',
             'my_discount',
+            'url',
+            'edit_url',
         ]
+
+    def get_edit_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={'pk': obj.pk}, request=request)
 
     def get_my_discount(self, obj):
         if not hasattr(obj, 'id'):
